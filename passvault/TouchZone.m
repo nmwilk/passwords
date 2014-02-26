9a535edabdeb32e5a22cff3381328cf77d971b9a
Neil Wilkinson <neil.wilkinson@gmx.com>
2014-02-26 22:07:54 +0000

    Created basic UI.

---
+++ passvault/TouchZone.m
@@ -0,0 +1,22 @@
+//
+// Created by Neil on 26/02/2014.
+// Copyright (c) 2014 MeasuredSoftware. All rights reserved.
+//
+
+#import "TouchZone.h"
+
+
+@implementation TouchZone {
+
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.backgroundColor = [UIColor lightGrayColor];
+    }
+
+    return self;
+}
+
+@end
---
+++ passvault/ViewControllers/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  passvault
+//
+//  Created by Neil on 26/02/2014.
+//  Copyright (c) 2014 MeasuredSoftware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic, strong) IBOutlet UIBarButtonItem *addPasswordBtn;
+@end
