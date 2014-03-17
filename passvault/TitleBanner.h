16772707f07fc235309de116c57d9dfa79574d6c
Neil Wilkinson <neil@measuredsoftware.co.uk>
2014-03-17 21:45:49 +0000

    Removed labels from addpassword VC. Added title banner to main screen.

---
+++ passvault/TitleBanner.h
@@ -0,0 +1,10 @@
+//
+// Created by Neil on 17/03/2014.
+// Copyright (c) 2014 MeasuredSoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TitleBanner : UIView
+@end
